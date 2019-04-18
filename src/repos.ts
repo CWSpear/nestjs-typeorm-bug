a5015caeae5170def8448b96c9c1241526fdd632
Cameron Spear <cameronspear@gmail.com>
2019-04-18 08:54:02 -0700

    Remove unused files

--- src/repos.ts
+++
@@ -1,5 +0,0 @@
-import { AgencyRepository } from './agency/agency.repo';
-import { UserRepository } from './user/user.repo';
-
-// simple way to avoid cyclical deps is not to import these repos into `app.module.ts`
-export const repos = [AgencyRepository, UserRepository];
